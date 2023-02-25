3d5724802a4bf0404c77dbcdb7038793a110e802
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-25 20:25:43 +0600

    client side functionality

--- components/themeSwitcher/themeSwitcher.tsx
+++ components/themeSwitcher/themeSwitcher.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
 
-function ThemeSwitcher() {
-    const [darkMode, setDarkMode] = useState(false);
+function checkTheme() {
+    if ( typeof localStorage === 'undefined' ) {
+        return false;
+    }
+    const darkMode = localStorage.getItem('darkMode');
+    return darkMode === null ? false : JSON.parse(darkMode)
+}
 
+function ThemeSwitcher() {
+    const [darkMode, setDarkMode] = useState<boolean>(checkTheme);
+    const handleClick = () => {
+        localStorage.setItem('darkMode', JSON.stringify(!darkMode))
+        setDarkMode(!darkMode);
+    }
     return (
         <>
-            <button onClick={() => setDarkMode(!darkMode)}>{
+            <button onClick={handleClick} suppressHydrationWarning>{
                 darkMode ? 'Light Mode' : 'Dark Mode'
             }</button>
 
             {
-                darkMode ? <style jsx global>
+                darkMode && <style jsx global>
                 {`
                 :root{
                         --background-color: rgb(21, 21, 21);
@@ -19,7 +30,7 @@ function ThemeSwitcher() {
                     }
                 `
                 }
-            </style> : null
+            </style> 
             }
         </>
     )
