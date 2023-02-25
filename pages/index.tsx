088fea9950f23974cf9fdba29269d65a2a23bb5b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-25 15:10:08 +0600

    blog learning next js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      
-      <main>
-        hello
-      </main>
-    </>
+    <div>
+      <h1>
+        My Blog
+      </h1>
+      <ul>
+        <li>
+          <Link href='/posts/first-post '>
+            Post
+          </Link>
+        </li>
+      </ul>
+    </div>
   )
 }
---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,10 @@
+function FirstPost() {
+    return(
+        <>
+            <h1>First Post</h1>
+            <p> this is my first ever blog post</p>
+        </>
+    )
+}
+
+export default FirstPost;
