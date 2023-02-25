7d3405ce7c9d6ebc44c44defa25c9dd2c01ffc43
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-25 19:54:58 +0600

    client side functionality

--- components/themeSwitcher/themeSwitcher.tsx
+++ components/themeSwitcher/themeSwitcher.tsx
@@ -4,11 +4,23 @@ function ThemeSwitcher() {
     const [darkMode, setDarkMode] = useState(false);
 
     return (
-
         <>
             <button onClick={() => setDarkMode(!darkMode)}>{
                 darkMode ? 'Light Mode' : 'Dark Mode'
             }</button>
+
+            {
+                darkMode ? <style jsx global>
+                {`
+                :root{
+                        --background-color: rgb(21, 21, 21);
+                        --link-color: rgb(228, 207, 9);
+                        --text-color: rgb(219, 217, 217);
+                    }
+                `
+                }
+            </style> : null
+            }
         </>
     )
 }
