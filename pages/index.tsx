9080a82f96ba193fea250495afe696a62c8c917d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-25 18:44:36 +0600

    get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,33 @@
 import { getSlug } from "@/libs/posts"
+import Link from "next/link";
+
+export async function getStaticProps() {
+  const slugs = await getSlug();
+  return{
+    props: {
+      paths: slugs.map((slug) => slug), 
+    fallback: false
+    }
+}
+}
 
 export default function Home(props: any) {
-  console.log(props)
+  console.log(props.paths)
   return (
     <div>
       <h1>
         My Blog
       </h1>
-      {/* <ul>
-        <li>
-          <Link href='/posts/first-post '>
-            Post 1
+      <ul>
+      {
+        props.paths.map((path: any, index: number) => 
+        <li key={index}>
+          <Link href={`/posts/${path}`}>
+          {path}
           </Link>
-        </li>
-        <li>
-          <Link href='/posts/second-post '>
-            Post 2
-          </Link>
-        </li>
-      </ul> */}
+        </li>)
+      }
+      </ul>
     </div>
   )
 }
