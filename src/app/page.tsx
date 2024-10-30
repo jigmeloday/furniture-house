eb82318413a0d4e2df3755bb0184272b99860a88
Jigme Lodey <jigmenlodey@gmail.com>
2024-10-30 10:17:16 +0600

    feat: button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <main>hello</main>
+    <main>
+      <Button > hello</Button>
+    </main>
   );
 }
