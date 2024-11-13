feadc5a99e3767b97357ea9398682df1f95c3e74
Jigme Lodey <jigmenlodey@gmail.com>
2024-11-13 18:01:28 +0600

    fix: pagination

--- src/app/shop/components/shop-item-list.tsx
+++ src/app/shop/components/shop-item-list.tsx
@@ -12,12 +12,15 @@ function ShopItemList({
 }) {
   return (
     <div className="px-[16px] sm:px-[85px] pt-[46px] pb-[85] space-y-[24px] mb-[24px]">
-      <div className="grid grid-cols md:grid-cols-4">
+      <div className="grid grid-cols md:grid-cols-4 gap-8">
         {data?.map((item) => (
           <div key={item.item_id}>
             <ShopCard item={item as PopularOrder & ShopItem} user={user} />
           </div>
         ))}
+      </div>
+      <div className="flex justify-center">
+        <Button variant="outline">Load More</Button>
       </div>
     </div>
   );
